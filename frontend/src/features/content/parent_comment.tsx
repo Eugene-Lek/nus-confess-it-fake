05511cdd9da9d9423e90442f434e0be17cb220d5
eugenelek <eugene.lek@ntucenterprise.sg>
2024-12-18 10:58:50 +0800

    feat: scroll to parent on click

--- frontend/src/features/content/parent_comment.tsx
+++ frontend/src/features/content/parent_comment.tsx
@@ -9,8 +9,15 @@ export interface ParentComment {
 }
 
 export const ParentCommentCard = (parentComment: ParentComment) => {
+
+    // Scrolls the page to the actual instance of the parent comment
+    const onClick = () => {
+        const commentElement = document.getElementById(parentComment.id);
+        commentElement?.scrollIntoView({ behavior: "smooth", block: "center" })
+    }
+
     return (
-        <Box sx={{borderLeft: 3, borderColor: "#F3A953", px: 3, my: 1}}>
+        <Box sx={{borderLeft: 3, borderColor: "#F3A953", px: 3, my: 1, cursor:"pointer"}} onClick={onClick}>
             <Typography variant="body2" fontWeight={"bold"}>{`${parentComment.author} `}</Typography>
             <Typography variant="body2" sx={{
                     overflow: "hidden",
