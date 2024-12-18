713540fce178b58d92f9c962ba89bde94db1e03e
eugenelek <eugene.lek@ntucenterprise.sg>
2024-12-18 10:58:13 +0800

    update: next config

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,8 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  transpilePackages: ['@mdxeditor/editor'],
   reactStrictMode: true,
+  webpack: (config) => {
+    // this will override the experiments
+    config.experiments = { ...config.experiments, topLevelAwait: true }
+    // this will just update topLevelAwait property of config.experiments
+    // config.experiments.topLevelAwait = true
+    return config
+  }
 };
 
 export default nextConfig;
