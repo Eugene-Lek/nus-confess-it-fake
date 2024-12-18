d01dd47f0b2380b5e87f56d2f013db06a152b200
Eugene-Lek <lekwceugene@gmail.com>
2024-12-18 10:58:29 +0800

    update: comment card

--- frontend/src/features/content/comment_card.tsx
+++ frontend/src/features/content/comment_card.tsx
@@ -18,12 +18,7 @@ export interface Comment {
     parentBody: string,
 }
 
-interface props {
-    comment: Comment,
-    setReplyTo: Dispatch<SetStateAction<Comment | undefined>>
-}
-
-export const CommentCard: FC<props> = ({comment, setReplyTo}) => {
+const CommentCardCore = (comment: Comment) =>  {
     const parentComment = {
         id: comment.parentId, 
         author: comment.parentAuthor, 
@@ -31,19 +26,43 @@ export const CommentCard: FC<props> = ({comment, setReplyTo}) => {
     }
 
     return (
-        <Box sx={{display: "flex", flexDirection: "column", gap: "5px", border: 1.7, borderRadius:10, borderColor: "#d3d3d3", py: 1, px: 5}}>
+        <>
             <Box sx={{display: "flex"}}>
                 <Typography variant="body2" fontWeight={"bold"}>{`${comment.author} `}</Typography>
                 <Typography variant="body2" color="grey">{`, ${formatDate(new Date(comment.createdAt))}`}</Typography>
             </Box>
             {comment.parentId ? <ParentCommentCard {...parentComment}/> : <></> }
             <Typography variant="body2">{comment.body}</Typography>
+        </>
+    )
+}
+
+interface props {
+    comment: Comment,
+    setReplyTo: Dispatch<SetStateAction<Comment | undefined>>
+}
+
+export const CommentCardUnderPost: FC<props> = ({comment, setReplyTo}) => {
+    return (
+        <Box id={comment.id} sx={{display: "flex", flexDirection: "column", gap: "5px", border: 1.7, borderRadius:10, borderColor: "#d3d3d3", py: 1, px: 5}}>
+            <CommentCardCore {...comment}/>
             <Box sx={{display: "flex", flexDirection: "row", gap: "25px"}}>
                 <VoteBox {...comment}/>
                 <Box onClick={() => {setReplyTo(comment)}} sx={{display: "flex", flexDirection: "row", gap: "5px",cursor: 'pointer'}}>
                     <Typography variant="body2" fontWeight={"bold"}>Reply</Typography>
                     <ReplyIcon/>
                 </Box>
+            </Box>
+        </Box>
+    )
+}
+
+export const CommentCardInListing = (comment: Comment) => {
+    return (
+        <Box id={comment.id} sx={{display: "flex", flexDirection: "column", gap: "5px", border: 1.7, borderRadius:10, borderColor: "#d3d3d3", py: 1, px: 5}}>
+            <CommentCardCore {...comment}/>
+            <Box sx={{display: "flex", flexDirection: "row", gap: "25px"}}>
+                <VoteBox {...comment}/>
             </Box>
         </Box>
     )
