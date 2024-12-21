78b45e8726d0f679003c20b9b4b38ee57f34438a
Eugene-Lek <lekwceugene@gmail.com>
2024-12-21 13:41:50 +0800

    update: implementation

--- backend/httperror/httperror.go
+++ backend/httperror/httperror.go
@@ -1,21 +1,30 @@
 package httperror
 
 import (
+	"fmt"
 	"net/http"
+
+	"github.com/pkg/errors"
 )
 
 // Define a custom error in order to track the http status of the error too
 // Code refers to a custom error code
 type Error struct {
-	Status int
-	Code string
+	Status  int
+	Code    string
+	Message string
 }
 
 func (e *Error) Error() string {
-	return e.Code
+	return e.Message
 }
 
-var InternalServerError = &Error{
-	Status: http.StatusInternalServerError,
-	Code: "INTERNAL-SERVER-ERROR",
+func NewInternalServerError(err error) *Error {
+	err = errors.New(err.Error()) // wraps the original error in a pkg/errors error. This way, the stack trace is included
+
+	return &Error{
+		Status:  http.StatusInternalServerError,
+		Message: fmt.Sprintf("%+v", err), // include the stack trace in the error message
+		Code:    "INTERNAL-SERVER-ERROR",
+	}
 }
