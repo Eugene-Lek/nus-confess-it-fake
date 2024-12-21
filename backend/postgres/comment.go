626715821c3235c2caae2fae002017bca24c9633
Eugene-Lek <lekwceugene@gmail.com>
2024-12-21 13:42:52 +0800

    multiple fixes

--- backend/postgres/comment.go
+++ backend/postgres/comment.go
@@ -2,45 +2,58 @@ package postgres
 
 import (
 	"backend/httperror"
+	"database/sql"
 	"fmt"
 	"strings"
 	"time"
 )
 
 type Comment struct {
-	id        string
-	body      string
-	author    string
-	postId    string
-	parentId  string
-	status    Status
-	likes     int
-	dislikes  int
-	createdAt string
-	updatedAt string
+	Id        string
+	Body      string
+	Author    string
+	PostId    string
+	ParentId  string
+	Status    string
+	Likes     int
+	Dislikes  int
+	CreatedAt string
+	UpdatedAt string
 }
 
 type CommentVote struct {
-	viewer string
-	commentId string
-	vote string
+	Viewer string
+	CommentId string
+	Vote string
 }
 
 func (postgres *PostgresStore) CreateComment(comment Comment) error {
-	query := `
-		INSERT INTO comment (id, body, author, post_id, status, parent_id) 
-		VALUES ($1, $2, $3, $4, $5, $6)`
-	_, err := postgres.db.Exec(query, comment.id, comment.body, comment.author, comment.postId, comment.status, comment.parentId)
+	// Parent id is optional, so let it be null if parent id is not provided
+
+	var err error
+	if (comment.ParentId == "") {
+		query := `
+			INSERT INTO comment (id, body, author, post_id, status) 
+			VALUES ($1, $2, $3, $4, $5)`
+		_, err = postgres.db.Exec(query, comment.Id, comment.Body, comment.Author, comment.PostId, comment.Status)
+	} else {
+		query := `
+			INSERT INTO comment (id, body, author, post_id, status, parent_id) 
+			VALUES ($1, $2, $3, $4, $5, $6)`
+		_, err = postgres.db.Exec(query, comment.Id, comment.Body, comment.Author, comment.PostId, comment.Status, comment.ParentId)
+	}
+
 	return checkPostgresErr(err)
 }
 
 // Get comments by post, author, search query, and who liked them (all optional) 
 // and sort by either newest, mosts likes, or relevance (default is by relevance)
 // (relevance is by search query. If search query is empty, then sort by newest first)
-func (postgres *PostgresStore) GetComments(author string, searchQuery string, likedBy string, sortBy string) ([]Comment, error) {
-	query := `SELECT (c.id, c.body, c.author, c.post_id, c.parent_id, c.status
-					  COUNT(comment_vote.like) AS likes, COUNT(comment_vote.dislike) AS dislikes,
-					  c.created_at, c.updated_at)
+func (postgres *PostgresStore) GetComments(postId string, author string, status string, searchQuery string, likedBy string, sortBy string) ([]Comment, error) {
+	query := `SELECT c.id, c.body, c.author, c.post_id, c.parent_id, c.status,
+					  COUNT(case when comment_vote.vote = 'Like' then 1 else null end) AS likes,
+					  COUNT(case when comment_vote.vote = 'Dislike' then 1 else null end) AS dislikes,
+					  c.created_at, c.updated_at
 			  FROM comment AS c
 			  LEFT JOIN comment_vote ON comment_vote.comment_id = c.id
 			  WHERE 1 = 1`
@@ -48,13 +61,23 @@ func (postgres *PostgresStore) GetComments(author string, searchQuery string, li
 	// Append conditions to the query based on the arguments provided
 	conditionCount := 1
 	conditions := []any{}
+	if (postId != "") {
+		query = query + fmt.Sprintf(" AND c.post_id = $%v", conditionCount)
+		conditions = append(conditions, postId)
+		conditionCount += 1
+	}
 	if (author != "") {
 		query = query + fmt.Sprintf(" AND c.author = $%v", conditionCount)
 		conditions = append(conditions, author)
 		conditionCount += 1
 	}
+	if (status != "") {
+		query = query + fmt.Sprintf(" AND c.status = $%v", conditionCount)
+		conditions = append(conditions, status)
+		conditionCount += 1
+	}
 	if (strings.Trim(searchQuery, " ") != "") {
-		query = query + fmt.Sprintf(" AND c.textsearchable_index @@ to_tsquery($%v)", conditionCount)
+		query = query + fmt.Sprintf(" AND c.textsearchable_index @@ plainto_tsquery($%v)", conditionCount)
 		conditions = append(conditions, searchQuery)
 		conditionCount += 1
 	}
@@ -70,12 +93,12 @@ func (postgres *PostgresStore) GetComments(author string, searchQuery string, li
 	case "newest":
 		query = query + " ORDER BY c.created_at DESC"
 	case "popular":
-		query = query + " ORDER BY c.likes DESC"
+		query = query + " ORDER BY likes DESC"
 	case "relevance":
 		// If search query is empty, sort by tag relevance
 		// If tags are empty, sort by newest first
 		if (searchQuery != "") {
-			query = query + fmt.Sprintf(` ORDER BY ts_rank_cd(c.textsearchable_index, to_tsquery(%s)) DESC`, searchQuery)
+			query = query + fmt.Sprintf(` ORDER BY ts_rank_cd(c.textsearchable_index, plainto_tsquery(%s)) DESC`, searchQuery)
 		} else {
 			query = query + " ORDER BY c.created_at DESC"
 		}
@@ -84,7 +107,7 @@ func (postgres *PostgresStore) GetComments(author string, searchQuery string, li
 	// Execute the query
 	rows, err := postgres.db.Query(query, conditions...)
 	if err != nil {
-		return nil, httperror.InternalServerError
+		return nil, httperror.NewInternalServerError(err)
 	}
 	defer rows.Close()
 
@@ -92,16 +115,18 @@ func (postgres *PostgresStore) GetComments(author string, searchQuery string, li
 
 	for rows.Next() {
 		var comment Comment
+		var parentId sql.NullString
 
 		err := rows.Scan(
-			&comment.id, &comment.body, &comment.author, &comment.postId, 
-			&comment.parentId, &comment.status, 
-			&comment.likes, &comment.dislikes, &comment.createdAt, &comment.updatedAt)
+			&comment.Id, &comment.Body, &comment.Author, &comment.PostId, 
+			&parentId, &comment.Status, 
+			&comment.Likes, &comment.Dislikes, &comment.CreatedAt, &comment.UpdatedAt)
 
 		err = checkPostgresErr(err)
 		if (err != nil) {
 			return nil, err
 		} else {
+			comment.ParentId = parentId.String
 			comments = append(comments, comment)
 		}
 	}
@@ -113,15 +138,24 @@ func (postgres *PostgresStore) UpdateComment(comment Comment) error {
 	query := `
 		UPDATE comment SET body = $1, post_id = $2, status = $3, parent_id = $4, updated_at = $5 
 		WHERE id = $6`
-	_, err := postgres.db.Exec(query, comment.body, comment.postId, comment.status, comment.parentId, time.Now(), comment.id)
+	_, err := postgres.db.Exec(query, comment.Body, comment.PostId, comment.Status, comment.ParentId, time.Now(), comment.Id)
 	return checkPostgresErr(err)
 }
 
-func (postgres *PostgresStore) CreateCommentVote(postVote PostVote) error {
+func (postgres *PostgresStore) SoftDeleteComment(postId string) error {
+	// Set the status to 'deleted' and clear the body
 	query := `
-		INSERT INTO comment_vote (viewer, comment_id, vote) 
-		VALUES ($1, $2, $3)`
-	_, err := postgres.db.Exec(query, postVote.viewer, postVote.postId, postVote.vote)
-	
+		UPDATE post SET body = '', status = 'Deleted', updated_at = $1 
+		WHERE id = $2`
+	_, err := postgres.db.Exec(query, time.Now(), postId)
+	return checkPostgresErr(err)
+}
+
+func (postgres *PostgresStore) UpsertCommentVote(commentVote CommentVote) error {
+	// Create the vote or update it if it already exists
+	query := `
+		INSERT INTO comment_vote (viewer, comment_id, vote) VALUES ($1, $2, $3)
+		ON CONFLICT(viewer, post_id) DO UPDATE SET vote = $3`
+	_, err := postgres.db.Exec(query, commentVote.Viewer, commentVote.CommentId, commentVote.Vote)
 	return checkPostgresErr(err)
 }
