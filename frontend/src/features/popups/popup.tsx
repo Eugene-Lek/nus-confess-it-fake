4e0917d4c003580154ab6e5e900886d9725c0ce8
Eugene-Lek <lekwceugene@gmail.com>
2024-12-26 22:20:47 +0800

    update: add delete confirmation component

--- frontend/src/features/popups/popup.tsx
+++ frontend/src/features/popups/popup.tsx
@@ -1,38 +1,43 @@
-import { Dialog} from "@mui/material";
-import { FC } from "react";
+import { Dialog, PaperProps} from "@mui/material";
+import { ElementType, FC, JSX, useContext } from "react";
 import { useAppSelector, useAppDispatch } from "@/redux/hooks";
 import { closed } from "./popup_slice";
-import { Login, submitLogin } from "./login";
-import { SignUp, submitSignup } from "./signup";
+import { Login } from "./login";
+import { SignUp } from "./signup";
+import { Error } from "./error";
+import { DeleteConfirmation } from "./delete_confirmation";
+import { PopUpContext } from "./popup_context";
 
 
-
+// This component is meant to be a singleton to ensure that at most 1 popup can exist at all times
 export const PopUp: FC = () => {
+    const open = useAppSelector((state) => state.popup.open)
     const type = useAppSelector((state) => state.popup.type)
     const dispatch = useAppDispatch()
     const handleClose = () => dispatch(closed())
 
+    const {deleteQueryFunc} = useContext(PopUpContext)
+    const resourceId = useAppSelector((state) => state.popup.props.resourceId)
+    const isDeleting = useAppSelector((state) => state.popup.props.isDeleting)
+
     return (
         <Dialog
-        open={type != "closed"}
+        open={open}
         onClose={handleClose}
         fullWidth
         maxWidth="xs"
-        PaperProps={{
-                component: 'form',
-                onSubmit: async(e: React.FormEvent<HTMLFormElement>) => {
-                    if (type == "login") {
-                        submitLogin(e, dispatch)
-                    } else {
-                        submitSignup(e, dispatch)
-                    }                           
-                }}
-            }
       >
         {type == "login" 
             ? <Login/> 
             : type == "signup"
             ? <SignUp/>
+            : type == "delete confirmation"
+            ? <DeleteConfirmation 
+                  deleteQueryFunc={deleteQueryFunc}
+                  resourceId={resourceId}
+                  isDeleting={isDeleting}/>
+            : type == "error"
+            ? <Error/>
             : <></>}
       </Dialog>
     )
