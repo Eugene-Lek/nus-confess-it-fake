ad39b6e1eb69378e15455998de9105b9f722d3ae
eugenelek <eugene.lek@ntucenterprise.sg>
2024-12-26 22:24:01 +0800

    new: my posts page

---
+++ frontend/src/pages/my-posts.tsx
@@ -0,0 +1,20 @@
+import { PostCards } from "@/features/content/posts/post_cards";
+import { useAppDispatch} from "@/redux/hooks";
+import { useGetMyPostsQuery } from "@/features/content/posts/api_slice";
+import { getUser, userIsLoggedIn } from "@/features/auth/auth";
+import { clickedLogin } from "@/features/popups/popup_slice";
+
+export default function MyPosts() {
+    const dispatch = useAppDispatch()
+
+    // Direct the user to login if they have not done so
+    const authenticated = userIsLoggedIn()
+    if (!authenticated) {
+        dispatch(clickedLogin())
+        return <></>
+    } 
+
+  return (
+      <PostCards apiQueryHook={useGetMyPostsQuery} extraProps={{author: getUser()}}/>
+  )
+}
