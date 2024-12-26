debd2df34ec9fa0b63bfdd7f9b63942b3de875f5
eugenelek <eugene.lek@ntucenterprise.sg>
2024-12-26 22:12:52 +0800

    fix: make scrollbar permenant

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,5 +1,7 @@
 import type { AppProps } from "next/app";
 import dynamic from 'next/dynamic'
+import '../styles/global.css'; // Used to make the scroll bar permenant
+
 
 // Root layout is defined separately instead of inside the _app file so that 
 // it can be imported in the _app file as a non-ssr component
@@ -7,7 +9,7 @@ import dynamic from 'next/dynamic'
 // because the dev server always prerenders components but some components rely on
 // localStorage, which only exists in the client.
 
-const NoSSRRootAppLayout = dynamic(() => import("@/features/global/RootAppLayout"), { ssr: false })
+const NoSSRRootAppLayout = dynamic(() => import("@/features/root/RootAppLayout"), { ssr: false })
 
 export default function App({ Component: Body, pageProps }: AppProps) {
   return <NoSSRRootAppLayout Component={Body} pageProps={pageProps}/>
