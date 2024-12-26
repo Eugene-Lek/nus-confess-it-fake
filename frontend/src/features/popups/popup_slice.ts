f31523cf5a6af58ae9da13d00d8f0b6e298fd982
Eugene-Lek <lekwceugene@gmail.com>
2024-12-26 22:20:34 +0800

    update: popup state & prop management

--- frontend/src/features/popups/popup_slice.ts
+++ frontend/src/features/popups/popup_slice.ts
@@ -1,15 +1,20 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit'
+import { TypedMutationTrigger, TypedUseMutation } from '@reduxjs/toolkit/query/react'
+import { useDeleteCommentMutation } from '../content/comments/api_slice'
+import { deleteConfimationProps } from './delete_confirmation'
 
 interface PopupState {
-    type: "closed" | "error" | "login" | "signup"
-    loading: boolean
+    type: "error" | "login" | "signup" | "delete confirmation" | undefined
+    open: boolean
     error: undefined | string
+    props: any
 }
 
 const initialState: PopupState = {
-    type: "closed",
-    loading: false,
+    type: undefined,
+    open: false,
     error: "Invalid username or password",
+    props: {},
 }
 
 export const popupSlice = createSlice({
@@ -22,30 +27,40 @@ export const popupSlice = createSlice({
         // which detects changes to a "draft state" and produces a brand new
         // immutable state based off those changes.
         // Also, no return statement is required from these functions.
-        state.type = "closed"
-        state.loading = false
+        state.type = undefined
+        state.open = false
         state.error = undefined
       },
       clickedLogin: (state) => {
         state.type = "login"
-        state.loading = false
+        state.open = true
         state.error = undefined
       },
       clickedSignup: (state) => {
         state.type = "signup"
-        state.loading = false
+        state.open = true
         state.error = undefined
       },
-      clickedSubmit: (state) => {
-        state.loading = true
+      clickedDelete: (state, action: PayloadAction<{isDeleting: boolean, resourceId: string}>) => {
+        state.type = "delete confirmation"
+        state.open = true
+        state.error = undefined
+        state.props = action.payload
       },
       errorOccured: (state, action: PayloadAction<string>) => {
         state.error = action.payload
+
+        // If popup is currently open, display the error on the existing popup
+        // Otherwise, open the default error popup
+        if (!state.open) {
+            state.type =  "error"
+            state.open = true
+        }
       },
     },
   })
   
 // Action creators are generated for each case reducer function
-export const { closed, clickedLogin, clickedSignup, clickedSubmit, errorOccured } = popupSlice.actions
+export const { closed, clickedLogin, clickedSignup, clickedDelete, errorOccured } = popupSlice.actions
 
 export default popupSlice.reducer
