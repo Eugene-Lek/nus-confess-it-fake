b052f45bead4e09feda8de80c482560fa2d6945d
eugenelek <eugene.lek@ntucenterprise.sg>
2024-12-26 22:16:57 +0800

    move: custom components

---
+++ frontend/src/features/content/custom_components.tsx
@@ -0,0 +1,49 @@
+import { TextareaAutosize as BaseTextareaAutosize } from '@mui/base/TextareaAutosize';
+import Markdown from 'react-markdown'
+import { styled } from '@mui/system';
+import { FC, PropsWithChildren } from 'react';
+import rehypeRaw from 'rehype-raw';
+import styles from "./content.module.css"
+
+export const Textarea = styled(BaseTextareaAutosize)(
+    () => `
+    font-family: Roboto, Helvetica, Arial, sans-serif;
+    font-weight: normal;
+    font-size: 0.875rem;
+    line-height: 1.43;
+    letter-spacing: 0.01071em;
+    padding: 8px 12px;
+    border-radius: 8px;
+ `,
+);
+
+export const MarkdownBody: FC<PropsWithChildren> = ({children}) => {
+    return (
+        <Markdown components={{
+            p(props) {
+                const {node, ...rest} = props
+                return <p className={styles.body} {...rest}/>
+            },
+            strong(props) {
+                const {node, ...rest} = props
+                return <strong className={styles.body} {...rest}/>
+            },
+            em(props) {
+                const {node, ...rest} = props
+                return <em className={styles.body} {...rest}/>
+            },
+            u(props) {
+                const {node, ...rest} = props
+                return <u className={styles.body} {...rest}/>
+            },
+            li(props) {
+                const {node, ...rest} = props
+                return <li className={styles.body} {...rest}/>
+            },
+            ol(props) {
+                const {node, ...rest} = props
+                return <ol style={{margin: 0}} {...rest}/>
+            }                        
+        }} rehypePlugins={[rehypeRaw]}>{String(children)}</Markdown>
+    )
+}
