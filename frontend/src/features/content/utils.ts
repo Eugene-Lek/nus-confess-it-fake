ddc3e40a31b36149e24219f4f25509e2df63cce0
Eugene-Lek <lekwceugene@gmail.com>
2024-12-26 22:16:42 +0800

    update: content utils

--- frontend/src/features/content/utils.ts
+++ frontend/src/features/content/utils.ts
@@ -1,17 +1,3 @@
-import { TextareaAutosize as BaseTextareaAutosize } from '@mui/base/TextareaAutosize';
-import { styled } from '@mui/system';
-
-export const Textarea = styled(BaseTextareaAutosize)(
-    () => `
-    font-family: Roboto;
-    font-size: 16px;
-    font-weight: normal;
-    line-height: 1.5;
-    padding: 8px 12px;
-    border-radius: 8px;
- `,
-);
-
 export const formatDate = (date: Date) => {
     const userLocale = Intl.NumberFormat().resolvedOptions().locale
     return (date).toLocaleString(userLocale, {
@@ -27,4 +13,12 @@ export const formatDate = (date: Date) => {
       // gives "PM".
       // This inconsistency leads to a react-hydration-error during
       // development
+}
+
+export const removeNewlines = (body: string) => {
+    return body.replaceAll("\n", " ")
+}
+
+export const convertToMarkdown = (body: string) => {
+    return body.replaceAll("\n", "\n\n")
 }
