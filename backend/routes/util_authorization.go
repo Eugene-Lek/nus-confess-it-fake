494b65a550eb180f75f595dffff5a4e0e90a35df
eugenelek <eugene.lek@ntucenterprise.sg>
2024-12-29 08:31:10 +0800

    refactor: move model definition into go file

--- backend/routes/util_authorization.go
+++ backend/routes/util_authorization.go
@@ -6,6 +6,20 @@ import (
 	"github.com/casbin/casbin/v2"
 )
 
+// We define the model here so that it will be included in the built executable
+var AuthModel = `
+[request_definition]
+r = sub, obj, act
+
+[policy_definition]
+p = sub, obj, act
+
+[policy_effect]
+e = some(where (p.eft == allow))
+
+[matchers]
+m = keyMatch(r.sub, p.sub) && keyMatch5(r.obj, p.obj) && r.act == p.act`
+
 func addAuthPolicies(policies [][]string, e casbin.IEnforcer) error {
 	_, err := e.AddPoliciesEx(policies) // Adds the policies to RAM for quick access
 	if err != nil {
